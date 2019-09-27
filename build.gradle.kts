1ac306ad49d3cfabd448dcce9764650254377ddb
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-09-27 08:08:03 -0400

    update Kotlin to 1.3.50 and JUnit from 4 to 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.3.50"
     `maven-publish`
 }
 
@@ -29,11 +29,18 @@ tasks.withType<KotlinCompile> {
     }
 }
 
+tasks.withType<Test> {
+    useJUnitPlatform {
+    }
+}
+
 dependencies {
     compile(kotlin("stdlib"))
 
-    testCompile(kotlin("test"))
-    testCompile("junit", "junit", "4.12")
+    testImplementation(kotlin("test"))
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = "5.5.1")
+    testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = "5.5.1")
+    testRuntimeOnly(group = "org.junit.platform", name = "junit-platform-launcher", version = "1.5.1")
 }
 
 
