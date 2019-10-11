501bd43ec006fc6ce7c4a9121a1e44dfa3cd42a0
Kate Brownbill <glowkate@gmail.com>
2019-10-11 16:37:54 -0400

    Added tests for Translation2D and Rotation2D

---
+++ src/test/kotlin/test/ca/warp7/frc/geometry/Rotation2DTest.kt
@@ -0,0 +1,24 @@
+package test.ca.frc.geometry
+
+import ca.warp7.frc.geometry.Rotation2D
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+//This is testing the Rotation2D class using unittesting
+
+class Rotation2DTest{
+
+    @Test
+    fun unaryMinusWorksProperly(){
+	val rotateTest = Rotation2D(0.89, 0.55).unaryMinus()
+	val rotateGolden = Rotation2D(0.89, -0.55)
+	assert(rotateTest.epsilonEquals(rotateGolden))
+    }
+	
+    @Test
+    fun inverseWorksProperly(){
+	val rotateTest = Rotation2D(0.9, 0.05).inverse
+	val rotateGolden = Rotation2D(0.9, -0.05)
+	assert(rotateTest.epsilonEquals(rotateGolden))
+    }
+}
