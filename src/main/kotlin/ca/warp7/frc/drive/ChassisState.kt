7aed0f6f6f43115355c1e95c2a4cfdcf1500fe0a
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-10-16 17:43:07 -0400

    add kinematic equations

--- src/main/kotlin/ca/warp7/frc/drive/ChassisState.kt
+++ src/main/kotlin/ca/warp7/frc/drive/ChassisState.kt
@@ -4,4 +4,21 @@ data class ChassisState(val linear: Double, val angular: Double) {
     override fun toString(): String {
         return "(${"%.3f".format(linear)}, ${"%.3f".format(angular)})"
     }
+
+
+    /**
+     * Solves the inverse kinematics of the drive train by converting linear
+     * and angular speed/acceleration into the speeds/accelerations of wheels
+     * on each side
+     *
+     * for velocity, solves (m/s, rad/s) into (m/s, m/s)
+     * for acceleration, solves (m/s^2, rad/s^2) into (m/s^2, m/s^2)
+     *
+     * @param wheelbaseRadius the wheel base radius
+     * @return the wheel state
+     */
+    fun solve(wheelbaseRadius: Double): WheelState = WheelState(
+            left = linear - angular * wheelbaseRadius,
+            right = linear + angular * wheelbaseRadius
+    )
 }
--- src/main/kotlin/ca/warp7/frc/drive/WheelState.kt
+++ src/main/kotlin/ca/warp7/frc/drive/WheelState.kt
@@ -8,4 +8,24 @@ data class WheelState(val left: Double, val right: Double) {
     operator fun times(by: Double) = WheelState(left * by, right * by)
 
     operator fun div(by: Double) = times(1.0 / by)
+
+    /**
+     * Solves the forward kinematics of the drive train by converting the
+     * speeds/accelerations of wheels on each side into linear and angular
+     * speed/acceleration
+     *
+     * Equations:
+     * v = (left + right) / 2
+     * w = (right - left) / L
+     *
+     * for velocity, solves (m/s, m/s) into (m/s, rad/s)
+     * for acceleration, solves (m/s^2, m/s^2) into (m/s^2, rad/s^2)
+     *
+     * @param wheelbaseRadius the wheelbase radius
+     * @return the chassis state
+     */
+    fun solve(wheelbaseRadius: Double): ChassisState = ChassisState(
+            linear = (left + right) / 2.0,
+            angular = (right - left) / (2 * wheelbaseRadius)
+    )
 }
