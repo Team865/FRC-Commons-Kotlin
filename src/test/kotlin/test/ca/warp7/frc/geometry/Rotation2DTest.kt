dba61806843c2ec1a38f9034cac8ec9166b4a5cb
Kate <glowkate@gmail.com>
2019-11-13 11:09:42 -0500

    Wrote aditional tests for Rotation 2D.

--- src/test/kotlin/test/ca/warp7/frc/geometry/Rotation2DTest.kt
+++ src/test/kotlin/test/ca/warp7/frc/geometry/Rotation2DTest.kt
@@ -14,6 +14,24 @@ class Rotation2DTest {
 //        assert(rotateTest.epsilonEquals(rotateGolden))
 //    }
 
+    fun scaledWorksProperly(){
+	val rotateTest =  Rotation2D(2.4, -23.2).scaled(-2.0)
+	val rotateGolden = Rotataion2D(-4.8, 46.4)
+	assert(rotateTest.epsilonEquals(rotateGolden))
+    }
+
+    fun timesWorksProperly(){
+	val rotateTest = Rotation2D(12.0, 2.0).times(3.0)
+	val rotateGolden = Rotation2D(36.0, 6.0)
+	assert(rotateTest.epsilonEquals(rotateGolden))
+    }
+
+    fun divWorksProperly(){
+	val rotateTest = Rotation2D(3.5, 2.4).div(2.0)
+	val rotateGolden = Rotation2D(1.75, 1.2)
+	assert(rotateTest.epsilonEquals(rotateGolden))
+    }
+
     @Test
     fun inverseWorksProperly() {
         val rotateTest = Rotation2D(0.9, 0.05).inverse
