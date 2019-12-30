64b31f9d0b0e55318432a6e51c88607551b93bf7
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-12-30 17:11:32 -0500

    implement equality tests and improve parameterizer

--- src/main/kotlin/ca/warp7/frc/trajectory/SplineParameterizer.kt
+++ src/main/kotlin/ca/warp7/frc/trajectory/SplineParameterizer.kt
@@ -6,6 +6,8 @@ import ca.warp7.frc.geometry.Pose2D
 import ca.warp7.frc.geometry.Rotation2D
 import ca.warp7.frc.path.QuinticSegment2D
 import ca.warp7.frc.path.quinticSplinesOf
+import java.util.*
+import kotlin.collections.ArrayList
 
 fun parameterizedSplinesOf(waypoints: List<Pose2D>): List<TrajectoryState> =
         quinticSplinesOf(waypoints).parameterized()
@@ -14,31 +16,36 @@ fun List<QuinticSegment2D>.parameterized(): List<TrajectoryState> {
     val points = mutableListOf<TrajectoryState>()
     val p0 = first().getState(0.0)
     points.add(TrajectoryState(p0.pose(), p0.curvature()))
-    forEach { points.addAll(it.parameterized()) }
+    forEach { points.addAll(parameterizeQuinticSegment(it)) }
     return points
 }
 
-fun QuinticSegment2D.parameterized(): List<TrajectoryState> {
-    val points = mutableListOf<TrajectoryState>()
-    parameterize(points, 0.0, 1.0)
-    return points
-}
+fun parameterizeQuinticSegment(segment: QuinticSegment2D): MutableList<TrajectoryState> {
+    val points = ArrayList<TrajectoryState>()
+    val stack = ArrayDeque<Double>()
+    var iterations = 0
+    stack.addLast(1.0)
+    stack.addLast(0.0)
+    while (stack.isNotEmpty()) {
+        val t0 = stack.removeLast()
+        val t1 = stack.removeLast()
+        val state = segment.getState(t1)
+        val statePose = state.pose()
+        val twist = (statePose - segment.getPose(t0)).log()
+        if (twist.dx > 0.127 || twist.dy > 0.00127 || twist.dTheta > 0.0872) {
+            val pivot = (t0 + t1) / 2.0
+            stack.addLast(t1)
+            stack.addLast(pivot)
+            stack.addLast(pivot)
+            stack.addLast(t0)
+        } else {
+            points.add(TrajectoryState(statePose, state.curvature()))
+        }
 
-
-fun QuinticSegment2D.parameterize(
-        points: MutableList<TrajectoryState>,
-        t0: Double,
-        t1: Double
-) {
-    val state = getState(t1)
-    val statePose = state.pose()
-    val twist = (statePose - getPose(t0)).log()
-    if (twist.dx > 0.127 || twist.dy > 0.00127 || twist.dTheta > 0.0872) {
-        parameterize(points, t0, (t0 + t1) / 2.0)
-        parameterize(points, (t0 + t1) / 2.0, t1)
-    } else {
-        points.add(TrajectoryState(statePose, state.curvature()))
+        iterations++
+        if (iterations > 5000) throw StackOverflowError("Spline cannot be parameterized")
     }
+    return points
 }
 
 /**
--- src/test/kotlin/ca/warp7/frc/trajectory/WPIEqualityTest.kt
+++ src/test/kotlin/ca/warp7/frc/trajectory/WPIEqualityTest.kt
@@ -4,16 +4,18 @@ import ca.warp7.frc.geometry.Pose2D
 import edu.wpi.first.wpilibj.geometry.Pose2d
 import edu.wpi.first.wpilibj.geometry.Rotation2d
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics
+import edu.wpi.first.wpilibj.trajectory.Trajectory
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator
 import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint
 import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveKinematicsConstraint
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class WPIEqualityTest {
     @Test
     fun testTrajectoryGeneratorEqual() {
-        val t1 = TrajectoryGenerator.generateTrajectory(listOf(Pose2d(), Pose2d(5.0, 0.0, Rotation2d())),
+        val t1 = TrajectoryGenerator.generateTrajectory(listOf(Pose2d(), Pose2d(5.0, 2.0, Rotation2d())),
                 TrajectoryConfig(3.0, 3.0)
                         .addConstraint(DifferentialDriveKinematicsConstraint(
                                 DifferentialDriveKinematics(2.0),
@@ -22,11 +24,24 @@ class WPIEqualityTest {
                         .addConstraint(CentripetalAccelerationConstraint(4.0))
         )
 
-        println(t1)
-
         val p = parameterizedSplinesOf(listOf(Pose2D(), Pose2D(5.0, 0.0, 0.0)))
         parameterizeTrajectory(p, 1.0, 3.0,
                 3.0, 4.0, Double.POSITIVE_INFINITY)
-        println(p.joinToString("\n"))
+
+        assertTrajectoryEqual(p, t1.states)
+    }
+
+    private fun assertTrajectoryEqual(t1: List<TrajectoryState>, t2: List<Trajectory.State>) {
+        assertEquals(t1.size, t2.size)
+        for (i in t1.indices) {
+            val s1 = t1[i]
+            val s2 = t2[i]
+            assertEquals(s1.v, s2.velocityMetersPerSecond, 1E6)
+            assertEquals(s1.curvature, s2.curvatureRadPerMeter, 1E6)
+            assertEquals(s1.pose.translation.x, s2.poseMeters.translation.x, 1E6)
+            assertEquals(s1.pose.translation.y, s2.poseMeters.translation.y, 1E6)
+            assertEquals(s1.pose.rotation.cos, s2.poseMeters.rotation.cos, 1E6)
+            assertEquals(s1.pose.rotation.sin, s2.poseMeters.rotation.sin, 1E6)
+        }
     }
 }
